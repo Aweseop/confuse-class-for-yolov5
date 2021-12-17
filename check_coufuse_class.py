47de88109da6890cd6ead3314ef692432ca8d928
Aweseop <87693860+Aweseop@users.noreply.github.com>
2021-12-17 15:36:21 +0900

    Update check_coufuse_class.py

--- check_coufuse_class.py
+++ check_coufuse_class.py
@@ -1,63 +1,61 @@
 import os
 import json
-import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
+import yaml
 import glob
 import tqdm
+import argparse
+import numpy as np
 
 import warnings
 warnings.filterwarnings(action='ignore')
 
 
-def get_confuse_json(label_path, save_path, save_file_name) :
+def get_confuse_json(label_path) :
     text_list = os.listdir(label_path)
     len(text_list)
-    inferences = {}
+    json_data = {}
 
     for text in text_list :
         with open(os.path.join(label_path, text), 'r') as file :
             text_id = text.replace('.txt', '')
-            inferences[text_id] = {}
+            json_data[text_id] = {}
             for line in file :
                 cat, xmin, ymin, xmax, ymax, conf = line.split()
                 xmin, ymin, xmax, ymax = map(str, map(lambda x: round(float(x), 2), [xmin, ymin, xmax, ymax]))
                 anno_id = '/'.join([xmin, ymin, xmax, ymax])
-                if anno_id not in inferences[text_id].keys() :
-                    inferences[text_id][anno_id] = {}
-                    inferences[text_id][anno_id]['gt_category_id'] = None
-                    inferences[text_id][anno_id]['1st_category_id'] = [cat, float(conf)]
-                    inferences[text_id][anno_id]['2nd_category_id'] = None
-                    inferences[text_id][anno_id]['3rd_category_id'] = None
-                    inferences[text_id][anno_id]['variance'] = 1
+                if anno_id not in json_data[text_id].keys() :
+                    json_data[text_id][anno_id] = {}
+                    json_data[text_id][anno_id]['gt_category_id'] = None
+                    json_data[text_id][anno_id]['1st_category_id'] = [cat, float(conf)]
+                    json_data[text_id][anno_id]['2nd_category_id'] = None
+                    json_data[text_id][anno_id]['3rd_category_id'] = None
+                    json_data[text_id][anno_id]['variance'] = 1
                 
                 else :
-                    if inferences[text_id][anno_id]['2nd_category_id'] is None :
-                        inferences[text_id][anno_id]['2nd_category_id'] = [cat, float(conf)]
-                        dev_1 = inferences[text_id][anno_id]['1st_category_id'][1] - inferences[text_id][anno_id]['2nd_category_id'][1]
+                    if json_data[text_id][anno_id]['2nd_category_id'] is None :
+                        json_data[text_id][anno_id]['2nd_category_id'] = [cat, float(conf)]
+                        dev_1 = json_data[text_id][anno_id]['1st_category_id'][1] - json_data[text_id][anno_id]['2nd_category_id'][1]
                         var = (dev_1)**2 / 2
-                        inferences[text_id][anno_id]['variance'] = var
-                    elif inferences[text_id][anno_id]['3rd_category_id'] is None :
-                        inferences[text_id][anno_id]['3rd_category_id'] = [cat, float(conf)]
-                        dev_1 = inferences[text_id][anno_id]['1st_category_id'][1] - inferences[text_id][anno_id]['2nd_category_id'][1]
-                        dev_2 = inferences[text_id][anno_id]['1st_category_id'][1] - inferences[text_id][anno_id]['3rd_category_id'][1]
+                        json_data[text_id][anno_id]['variance'] = var
+                    elif json_data[text_id][anno_id]['3rd_category_id'] is None :
+                        json_data[text_id][anno_id]['3rd_category_id'] = [cat, float(conf)]
+                        dev_1 = json_data[text_id][anno_id]['1st_category_id'][1] - json_data[text_id][anno_id]['2nd_category_id'][1]
+                        dev_2 = json_data[text_id][anno_id]['1st_category_id'][1] - json_data[text_id][anno_id]['3rd_category_id'][1]
                         var = ((dev_1)**2 + (dev_2)**2) / 3
-                        inferences[text_id][anno_id]['variance'] = var
+                        json_data[text_id][anno_id]['variance'] = var
                     else :
                         continue
 
-    print(f'json length : {len(inferences.keys())}')
-    with open(os.path.join(save_path, save_file_name), 'w') as f :
-        json.dump(inferences, f)
+    print(f'json length : {len(json_data.keys())}')
+    return json_data
 
 
-def load_json(json_path, data_path) :
-    with open(json_path, 'r') as f :
-        json_data = json.load(f)
-
+def edit_json(json_data, data_path) :
     img_list = list(json_data.keys())
     txt_path = glob.glob(os.path.join(data_path, '**', '*.txt'))
+
     # edit json data
+    print('Json editing...')
     for img in tqdm.tqdm(img_list) :
         json_len = len(list(json_data[img].keys()))
         for file in txt_path :
@@ -67,6 +65,27 @@ def load_json(json_path, data_path) :
                     for i in range(json_len) :
                         json_data[img][list(json_data[img].keys())[i]]['gt_category_id'] = gt
                 break
+    
+    return json_data, img_list
+
+
+def load_json(label_path, data_path, save_path, save_json_name) :
+    file_name = os.path.join(save_path, save_json_name)
+
+    if os.path.isfile(file_name) :
+        print(f'Using already exist json file : {file_name}')
+        print('\n')
+        with open(file_name, 'r') as f :
+            json_data = json.load(f)
+            img_list = list(json_data.keys())
+
+    else :
+        json_data = get_confuse_json(label_path)
+        json_data, img_list = edit_json(json_data, data_path)
+        with open(file_name, 'w') as f :
+            json.dump(json_data, f)
+            print(f'Complete save json! : {file_name}')
+            print('\n')
 
     return json_data, img_list
 
@@ -143,18 +162,27 @@ def dict_cleansing(gt_dict) :
     return gt_dict
 
 
-def show_confused_class(gt_dict, class_id) :
+def parse_name(yaml_path):
+    assert os.path.isfile(yaml_path) 
+    with open(yaml_path, 'r', encoding='utf-8') as f:
+        names = yaml.load(f, Loader=yaml.FullLoader)['names']
+        food_names = {str(id):name for id, name in enumerate(names)}
+
+        return food_names
+
+
+def show_confused_class(food_name, gt_dict, class_id) :
     try :
         class_id = str(class_id)
         if class_id == 'all' :
             class_id = list(gt_dict.keys())
         else :
             class_id = [class_id]
-
+            
         for id in class_id :
-            print(f'Confused class for {id}')
+            print(f'Confused class for {id} ({food_name[id]})')
             for i, element in enumerate(gt_dict[id]) :
-                print(f'Top {i+1} - Id : {element[0]}, Count : {element[1]["count"]}, Variance : {element[1]["variance"]}')
+                print(f'Top {i+1} - Id : {element[0]} ({food_name[element[0]]}), Count : {element[1]["count"]}, Variance : {element[1]["variance"]}')
             print('\n')
 
     except :
@@ -164,3 +192,30 @@ def show_confused_class(gt_dict, class_id) :
         else :
             pass
 
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--threshold', type=int, default=0.05, help='variance threshold')
+    parser.add_argument('--class_id', type=str, default='all', help='you can also enter a specific class id.')
+    parser.add_argument('--label_path', type=str, default='/opt/ml/final/yolov5/runs/val/exp6/labels', help='you should execute val.py with --save-txt, --save-conf option')
+    parser.add_argument('--data_path', type=str, default='/opt/ml/final/datasets/val', help='dataset path')
+    parser.add_argument('--save_path', type=str, default='/opt/ml/final/yolov5/custom_metric', help='json save path')
+    parser.add_argument('--save_json_name', type=str, default='output.json', help='json save name')
+    parser.add_argument('--yaml_path', type=str, default='/opt/ml/final/yolov5/data/coco.yaml', help='coco yaml path')
+
+    args = parser.parse_args()
+    return args
+
+
+def main(args) :
+    json_data, img_list = load_json(args.label_path, args.data_path, args.save_path, args.save_json_name)
+    confidence_variance = get_confidence_variance(json_data, img_list, threshold=args.threshold)
+    gt_dict = get_gt_dict(confidence_variance)
+    gt_dict = dict_cleansing(gt_dict)
+    food_name = parse_name(args.yaml_path)
+    show_confused_class(food_name, gt_dict, args.class_id)
+
+
+if __name__ == '__main__' :
+    args = parse_args()
+    main(args)
